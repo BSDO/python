b5895c4ba0f9dc3b9e21e66466ac62b3aa5370e0
Breno silva <brenosill@hotmail.com>
2022-09-30 18:22:05 -0300

    aulas adicionadas com explicação

---
+++ python basics/booleanos_conversoes.py
@@ -0,0 +1,17 @@
+# booleanos
+# verdadeiro ou falso
+
+nome = 'breno'
+nome2 = 'bruno'
+
+print(nome == nome2)
+#------------------------------------------------
+#conversoes
+""" --- tipos de comentarios
+ano = input("Digite o ano atual: ")
+sua_idade = input("Digite sua idade: ")
+ano_nasc = int(ano) - int(sua_idade)
+
+print(f'O ano do seu nasciemnto {ano_nasc}')
+
+"""
--- python basics/functionsmethodos.py
+++ python basics/functionsmethodos.py
@@ -1,6 +1,11 @@
-#funçoes com metodos integrados
-#len,join, etc...
-#print(len("Ola,o que gostaria de fazer?")) #len mostra o tamanho da string
+# funçoes com metodos integrados
+# len,join, ROUND etc...
+# print(len("Ola,o que gostaria de fazer?")) #len mostra o tamanho da string
 
-nome = "heloooooooo"
-print(nome.join("ANdre"))
+nome = "to be to be to be to be"
+print(nome[0:len(nome)])
+
+print(nome.upper())  # Passa para maiscula a frase
+print(nome.capitalize())  # primeira letra maiscula
+print(nome.find('be'))  # mapeia a palavra destacad
+print(nome.replace('be', antigo))  # repete pelo novo atributo
