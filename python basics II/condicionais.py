c0bfcb7c9115aa7a8f512eaa084ce5ef977d609c
Breno <brenosill@hotmail.com>
2022-10-23 19:54:49 -0300

    aulas sobre as condiçoes do if,elif e else

---
+++ python basics II/condicionais.py
@@ -0,0 +1,13 @@
+# logica condicional
+
+nome = input("Ola seja-bem vindo,como se chama para verifica no sistema: ")
+
+banco_dados = []
+banco_dados.append(nome)
+
+if nome  ==  banco_dados[0]:
+     print(f"certo {nome} vi que seu nome esta aqui!!")
+else:
+     print("Tudo errado")
+
+
